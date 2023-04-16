6e0026d404856a15b02680ac9cfdb8812be286a5
lidasharova <Lida050496@yandex.ru>
2023-04-17 01:47:42 +0700

    feat:completed the dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -2,22 +2,42 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  //проверка на массив
+  if (Array.isArray(members)) {
+    let arr = [];
+    for (let i = 0; i < members.length; i++) {
+      if (typeof members[i] == 'string') {
+        for (let j = 0; j < members[i].length; j++) {
+          if (members[i][j] !== ' ') {
+            arr.push(members[i][j].toUpperCase());
+            break;
+          }
+        }
+      }
+    }
+    return arr.sort().join('');
+  } else {
+    return false;
+  }
 }
 
 module.exports = {
-  createDreamTeam
+  createDreamTeam,
 };
+
+// members[i] !== null &&
+// members[i] !== undefined &&
+// typeof members[i] !== 'number' &&
+// typeof members[i] !== 'boolean'
